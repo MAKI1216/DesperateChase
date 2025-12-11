a246321d5210164f560f20e0f2a5a9777fad87d7
maki <2194237921@qq.com>
2025-12-11 21:09:18 +0800

    weapon-identify

---
+++ Source/DesperateChase/weapon/GunBaseClient.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DesperateChase/weapon/WeaponBaseClient.h"
+#include "GunBaseClient.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DESPERATECHASE_API AGunBaseClient : public AWeaponBaseClient
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/DesperateChase/weapon/GunBaseServer.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DesperateChase/weapon/WeaponBaseServer.h"
+#include "GunBaseServer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DESPERATECHASE_API AGunBaseServer : public AWeaponBaseServer
+{
+	GENERATED_BODY()
+	
+};
